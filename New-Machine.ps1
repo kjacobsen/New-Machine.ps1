7c1e423f7f0ec19ba4035127bb83c8ac1304938d
Kieran Jacobsen <code@poshsecurity.com>
2016-03-30 12:05:29 +1100

    added change to ensure appropriate email config happens

--- New-Machine.ps1
+++ New-Machine.ps1
@@ -35,7 +35,6 @@ Install-Package -Name 'ccleaner'
 Install-Package -Name 'VLC'
 Install-Package -Name 'WinSCP.install'
 Install-Package -Name 'rdcman'
-#Install-Package -Name 'irfanview'
 Install-Package -Name 'lastpass'
 Install-Package -Name 'skype'
 Install-Package -Name 'spotify'
@@ -43,7 +42,6 @@ Install-Package -Name 'CrashPlan'
 Install-Package -Name 'secunia.psi'
 Install-Package -Name 'tunnelier'
 Install-Package -Name 'nodejs.install'
-Install-Package -Name 'virtualbox'
 #gpg4win
 #AAD PowerShell
 
@@ -55,10 +53,10 @@ Install-Module AzureRM
 Install-AzureRM
 
 'Configuring EMET and system protections'
-& 'C:\Program Files (x86)\EMET 5.2\EMET_Conf.exe' --system --force DEP=AlwaysON SEHOP=AlwaysON ASLR=ApplicationOptIN Pinning=Enabled
-& 'C:\Program Files (x86)\EMET 5.2\EMET_Conf.exe' --import 'C:\Program Files (x86)\EMET 5.2\Deployment\Protection Profiles\CertTrust.xml' --force
-& 'C:\Program Files (x86)\EMET 5.2\EMET_Conf.exe' --import 'C:\Program Files (x86)\EMET 5.2\Deployment\Protection Profiles\Popular Software.xml' --force
-& 'C:\Program Files (x86)\EMET 5.2\EMET_Conf.exe' --import 'C:\Program Files (x86)\EMET 5.2\Deployment\Protection Profiles\Recommended Software.xml' --force
+& 'C:\Program Files (x86)\EMET 5.5\EMET_Conf.exe' --system --force DEP=AlwaysON SEHOP=AlwaysON ASLR=ApplicationOptIN Pinning=Enabled
+& 'C:\Program Files (x86)\EMET 5.5\EMET_Conf.exe' --import 'C:\Program Files (x86)\EMET 5.5\Deployment\Protection Profiles\CertTrust.xml'
+& 'C:\Program Files (x86)\EMET 5.5\EMET_Conf.exe' --import 'C:\Program Files (x86)\EMET 5.5\Deployment\Protection Profiles\Popular Software.xml'
+& 'C:\Program Files (x86)\EMET 5.5\EMET_Conf.exe' --import 'C:\Program Files (x86)\EMET 5.5\Deployment\Protection Profiles\Recommended Software.xml'
 'You will need to reboot as EMET has been configured'
 
 'Enable CTL+ALT+DEL at logon'
@@ -128,9 +126,8 @@ $SshHostKeysPath = 'HKCU:\SOFTWARE\SimonTatham\PuTTY\SshHostKeys'
 if (-not (Test-Path $SshHostKeysPath)) { New-Item $SshHostKeysPath -ItemType Directory -Force }
 Set-ItemProperty -Path $SshHostKeysPath -Name 'rsa2@22:github.com' -Value '0x23,0xab603b8511a67679bdb540db3bd2034b004ae936d06be3d760f08fcbaadb4eb4edc3b3c791c70aae9a74c95869e4774421c2abea92e554305f38b5fd414b3208e574c337e320936518462c7652c98b31e16e7da6523bd200742a6444d83fcd5e1732d03673c7b7811555487b55f0c4494f3829ece60f94255a95cb9af537d7fc8c7fe49ef318474ef2920992052265b0a06ea66d4a167fd9f3a48a1a4a307ec1eaaa5149a969a6ac5d56a5ef627e517d81fb644f5b745c4f478ecd082a9492f744aad326f76c8c4dc9100bc6ab79461d2657cb6f06dec92e6b64a6562ff0e32084ea06ce0ea9d35a583bfb00bad38c9d19703c549892e5aa78dc95e250514069'
 
-'Setting git identity'
-git.exe config --global user.name 'Kieran Jacobsen'
-git.exe config --global user.email 'code@poshsecurity.com'
+'Make Git ask who I am before I commit'
+git config --global user.useconfigonly true
 
 if ((& git.exe config push.default) -eq $null)
 {
