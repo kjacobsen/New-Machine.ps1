35b5882bbefe97929ef2ba01ac975bb5572bd359
Kieran Jacobsen <kjacobsen@users.noreply.github.com>
2017-03-28 11:24:37 +1100

    Update New-Machine.ps1
    remove emet

--- New-Machine.ps1
+++ New-Machine.ps1
@@ -1,4 +1,4 @@
-$ErrorActionPreference = 'Stop'
+ErrorActionPreference = 'Stop'
 
 'Setting up Execution Policy'
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted
@@ -21,7 +21,7 @@ $ChocolateySoftwareToInstall = @(
     'adobereader'
     'tunnelier'
     'CrashPlan'
-    'emet'
+    #'emet'
     'fiddler4'
     'git.install'
     'google-chrome-x64'
